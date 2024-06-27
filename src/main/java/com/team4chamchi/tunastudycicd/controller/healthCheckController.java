4e69192be59321f05a3a0a0ffd562456dd1d55c6
jaehyeon-SMU <cookeeholic@naver.com>
2024-06-27 16:31:56 +0900

    health check controller

---
+++ src/main/java/com/team4chamchi/tunastudycicd/controller/healthCheckController.java
@@ -0,0 +1,13 @@
+package com.team4chamchi.tunastudycicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "yammi";
+    }
+}

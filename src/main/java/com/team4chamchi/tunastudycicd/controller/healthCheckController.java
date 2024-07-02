9746694ef187aca504f05e98505d2d712a570c71
chosoobin37 <angelajsb@naver.com>
2024-07-02 23:25:11 +0900

    healthcheck update

--- src/main/java/com/team4chamchi/tunastudycicd/controller/healthCheckController.java
+++ src/main/java/com/team4chamchi/tunastudycicd/controller/healthCheckController.java
@@ -8,6 +8,6 @@ public class healthCheckController {
 
     @GetMapping("/")
     public String healthCheck() {
-        return "health check ver 003";
+        return "health check ver 004";
     }
 }

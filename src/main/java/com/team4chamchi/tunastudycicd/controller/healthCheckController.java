7e9f51cb5e307811f210f273f867118962449c49
chosoobin37 <angelajsb@naver.com>
2024-07-03 10:09:36 +0900

    Feature: NotificationService, Update: healthCheck update

--- src/main/java/com/team4chamchi/tunastudycicd/controller/healthCheckController.java
+++ src/main/java/com/team4chamchi/tunastudycicd/controller/healthCheckController.java
@@ -8,6 +8,6 @@ public class healthCheckController {
 
     @GetMapping("/")
     public String healthCheck() {
-        return "health check ver 006";
+        return "health check ver 007";
     }
 }
